9511d69e62c46bd9b8e11ec16e7a426949d8ef1d
ajv89 <aresrosari@gmail.com>
2025-06-27 22:30:56 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+
+// ESTO ES PARA COMENTARIO
+let miImage = document.querySelector("img");
+miImage.onclick = function () {
+  let miSrc = miImage.getAttribute("src");
+  if (miSrc ==="images/imag1.jpg") {
+    miImage.setAttribute("src", "images/cr.png");
+  } else {
+    miImage.setAttribute("src", "images/imag1.jpg");
+  }
+};
+let miBoton = document.querySelector("button");
+let miTitulo = document.querySelector("h1");
+function estableceNombreUsuario() {
+  let miNombre = prompt("Introduzca su nombre.");
+  if (!miNombre) {
+    estableceNombreUsuario();
+  } else {
+    localStorage.setItem("nombre", miNombre);
+    miTitulo.innerHTML = "1Que alegria el que vuevas, " + miNombre;
+  }
+};
+function estableceNombreUsuario() {
+  let miNombre = prompt("Por favor, ingresa tu nombre.");
+  localStorage.setItem("nombre", miNombre);
+  miTitulo.textContent = "Ingreso correcto," + miNombre;
+}
+if (!localStorage.getItem("nombre")) {
+  estableceNombreUsuario();
+} else {
+  let nombreAlmacenado = localStorage.getItem("nombre");
+  miTitulo.innerHTML = "Que alegria el que vuelvas," + nombreAlmacenado;
+}
+miBoton.onclick = function () {
+  estableceNombreUsuario();
+};
